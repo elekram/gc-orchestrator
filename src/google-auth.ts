c6268b2b60a1561e37b5f7b3f6e2c8b55988b545
Mark Lee <mlee.aus@gmail.com>
2022-11-15 10:46:54 +1100

    wip: add google apis and create google auth module

---
+++ src/google-auth.ts
@@ -0,0 +1,19 @@
+import { google } from './deps.ts'
+import appSettings from '../config/config.ts'
+
+const keyFile = await Deno.readTextFile(appSettings.keyFile)
+const keys = JSON.parse(keyFile)
+
+const scopes = appSettings.scopes
+
+export default function GoogleAut() {
+
+  const auth = new google.Auth.JWT({
+    email: keys.client_email,
+    key: keys.private_key,
+    scopes,
+    subject: 'superadmin@cheltsec.vic.edu.au'
+  })
+
+  return auth
+}
