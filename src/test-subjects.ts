a5ebffedd8800f6560fdc99dc8e242e662a03613
Mark Lee <mlee.aus@gmail.com>
2022-11-15 10:49:22 +1100

    wip: add module to test dataset and warn if isses are found

---
+++ src/test-subjects.ts
@@ -0,0 +1,77 @@
+import { tinyLogger } from './deps.ts'
+import { Subject } from './get-data-set.ts'
+import appSettings from '../config/config.ts'
+
+export default function testSubjects(subjects: Map<string, Subject>) {
+  const verboseWarnings = appSettings.verboseWarnings
+
+  for (const [key, s] of subjects) {
+    let isWarning = false
+
+    if (!s.domain) {
+      isWarning = true
+      const logLevel = 'warn'
+      const type = 'ƒ-testSubject'
+      const message = `Domain/Faculty not found in ${key} ${s.name}`
+
+      tinyLogger.log(type, message, {
+        logLevel,
+        fileName: './log/log.csv'
+      })
+    }
+
+    if (!s.leaders.size) {
+      isWarning = true
+      const logLevel = 'warn'
+      const type = 'ƒ-testSubject'
+      const message = `No domain leaders found in ${key} ${s.name}`
+
+      tinyLogger.log(type, message, {
+        logLevel,
+        fileName: './log/log.csv'
+      })
+    }
+
+    if (!s.teachers.size) {
+      isWarning = true
+      const logLevel = 'warn'
+      const type = 'ƒ-testSubject'
+      const message = `No teachers found in ${key} ${s.name}`
+
+      tinyLogger.log(type, message, {
+        logLevel,
+        fileName: './log/log.csv'
+      })
+    }
+
+    if (!s.classes.size) {
+      isWarning = true
+      const logLevel = 'warn'
+      const type = 'ƒ-testSubject'
+      const message = `No classes found in ${key} ${s.name}`
+
+      tinyLogger.log(type, message, {
+        logLevel,
+        fileName: './log/log.csv'
+      })
+    }
+
+    for (const [key, students] of s.classes) {
+      if (!students.size) {
+        isWarning = true
+        const logLevel = 'warn'
+        const type = 'ƒ-testSubject'
+        const message = `No students found in class ${key} ${s.name}`
+
+        tinyLogger.log(type, message, {
+          logLevel,
+          fileName: './log/log.csv'
+        })
+      }
+    }
+
+    if (verboseWarnings && isWarning) {
+      console.log(s)
+    }
+  }
+}
