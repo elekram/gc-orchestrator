c478edd9d70538a80054bea65eacb2d0ac8d4bdc
= <=>
2024-01-23 11:56:23 +1100

    tweak: cleanup and simplify args

--- src/args.ts
+++ src/args.ts
@@ -9,7 +9,7 @@ export function processArgs(rawArgs: string[]) {
     '--run-tasks',
     '--log-course-tasks',
     '--log-enrolment-tasks',
-    '--view-aliases',
+    '--view-course-aliases',
     '--view-subject',
     '--view-composites',
     '--replace-teacher',
@@ -47,27 +47,9 @@ export function processArgs(rawArgs: string[]) {
       '  Synchronise Google Classrom courses and course members to school Timetabler CSV files.\n',
     )
     console.log('%cFLAG EXAMPLES', 'color:yellow')
-    console.log(
-      '  --Run-Tasks           : Run tasks generated from timetable CSVs.\n',
-    )
-    console.log(
-      '  --Log-Course-Tasks   : Log generated course tasks to screen and file screen instead of executing them.\n',
-    )
-    console.log(
-      '  --Log-Enrolment-Tasks: Log generated enrolment tasks to screen and file instead of executing them.\n',
-    )
-    console.log(
-      '  --View-Aliases        : View course ID to alias map to screen.\n',
-    )
-    console.log(
-      '  --View-Subject        : View user defined subject.\n',
-    )
-    console.log(
-      '  --View-Composites     : View composite classes.\n',
-    )
-    console.log(
-      '  --Delete-Corse        : Delete Course.\n',
-    )
-    console.log('\n%c' + exceptionMsg + '\n', 'color:red')
+
+    for (const flag of acceptedFlags) {
+      console.log(`\n  ${flag.toUpperCase()}`)
+    }
   }
 }
